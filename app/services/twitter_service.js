de036d2c5f1579016ad590cb1b914fb48b67b121
Patrick Widmer <padi.widmer@gmail.com>
2018-05-02 16:15:22 +0200

    Prepare to support fetching tweets for multiple tags

--- app/services/twitter_service.js
+++ app/services/twitter_service.js
@@ -8,44 +8,47 @@ module.exports = {
     fetchTweets: (db) => {
         const tweetCollection = db.collection('tweets');
         const today = new Date().toISOString().slice(0, 10);
+        const tags = ['#crypto'];
 
-        TwitClient.get('search/tweets', {
-            q: '#crypto since:' + today,
-            count: 999999,
-            language: 'en'
-        }, async function (err, data, res) {
-            let statuses = data.statuses.map(
-                ({
-                    created_at,
-                    text,
-                    retweet_count,
-                    favorite_count,
-                    user: {
-                        followers_count,
-                        friends_count
-                    }
-                }) => ({
-                    created_at,
-                    text,
-                    retweet_count,
-                    favorite_count,
-                    user: {
-                        followers_count,
-                        friends_count
-                    },
-                    relevance: calculateRelevance(
+        tags.forEach((tag) => {
+            TwitClient.get('search/tweets', {
+                q: `${tag} since:${today}`,
+                count: 999999,
+                language: 'en'
+            }, async function (err, data, res) {
+                let statuses = data.statuses.map(
+                    ({
+                        created_at,
+                        text,
                         retweet_count,
                         favorite_count,
-                        followers_count,
-                        friends_count
-                    )
-                })
-            );
-            let scores = statuses.map(({ text }) => simpleAnalysis(text));
-            scores = await Promise.all(scores);
-            statuses = statuses.map(({ ...props }, i) => ({ ...props, score: scores[i] }));
-            tweetCollection.insertMany(statuses);
-            console.log('TWITTER SERVICE::INSERTED');
+                        user: {
+                            followers_count,
+                            friends_count
+                        }
+                    }) => ({
+                        created_at,
+                        text,
+                        retweet_count,
+                        favorite_count,
+                        user: {
+                            followers_count,
+                            friends_count
+                        },
+                        relevance: calculateRelevance(
+                            retweet_count,
+                            favorite_count,
+                            followers_count,
+                            friends_count
+                        )
+                    })
+                );
+                let scores = statuses.map(({ text }) => simpleAnalysis(text));
+                scores = await Promise.all(scores);
+                statuses = statuses.map(({ ...props }, i) => ({ ...props, score: scores[i] }));
+                tweetCollection.insertMany(statuses);
+                console.log('TWITTER SERVICE::INSERTED');
+            });
         });
     }
 };
