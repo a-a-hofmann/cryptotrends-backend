38b93bdfe95633cca08e9a886b30aeb66067f9f5
Patrick Widmer <padi.widmer@gmail.com>
2018-05-23 15:35:40 +0200

    Add cron job to daily fetch historical data from gdax will drop entire collection and fetch data from previous 600 days

---
+++ app/crons/historical_cron.js
@@ -0,0 +1,14 @@
+const CronJob = require('cron').CronJob;
+const { fetchHistoricalData } = require('../services');
+
+module.exports = function (db) {
+    const historicalCollection = db.collection('historical');
+
+    new CronJob('00 55 23 * *', function () {
+        historicalCollection.drop(function (err, delOK) {
+            if (delOK) {
+                fetchHistoricalData(db);
+            }
+        });
+    }, null, true, 'Europe/Zurich');
+};
--- app/crons/index.js
+++ app/crons/index.js
@@ -1,8 +1,10 @@
 const cryptoCron = require('./crypto_cron');
 const twitterCron = require('./twitter_cron');
+const historicalCron = require('./historical_cron');
 
 module.exports = function (db) {
     // cryptoCron(db);
     twitterCron(db);
+    historicalCron(db);
     // Other route groups could go here, in the future
 };
