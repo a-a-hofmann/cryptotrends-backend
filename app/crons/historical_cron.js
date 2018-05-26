d760b761c9832148f5d0ca190a7e819f0a3a20c2
Alexander Hofmann <alexhofmann@gmail.com>
2018-05-26 12:02:55 +0200

    Added a log message also when defining the cron job

--- app/crons/historical_cron.js
+++ app/crons/historical_cron.js
@@ -1,16 +1,20 @@
 const CronJob = require('cron').CronJob;
 const { fetchHistoricalData } = require('../services');
+const chalk = require('chalk');
 
 module.exports = function (db) {
     const historicalCollection = db.collection('historical');
 
-    new CronJob('00 55 23 * * *', function () {
-        const nextExecution = this.nextDates().toString();
-        console.log('HISTORICAL CRON::STARTED Will fire again at: ' + nextExecution);
+    const job = new CronJob('00 55 23 * * *', function () {
+        let nextExecution = this.nextDates().toString();
+        console.log('HISTORICAL CRON::STARTED. Will fire again at: ' + nextExecution);
         historicalCollection.drop(function (err, delOK) {
             if (delOK) {
                 fetchHistoricalData(db);
             }
         });
     }, null, true, 'Europe/Zurich');
+
+    let nextExecution = job.nextDates().toString();
+    console.log(chalk.green('HISTORICAL CRON::REGISTERED. Will fire at: ' + nextExecution));
 };
--- app/crons/twitter_cron.js
+++ app/crons/twitter_cron.js
@@ -1,10 +1,14 @@
 const CronJob = require('cron').CronJob;
 const { fetchTweets } = require('../services');
+const chalk = require('chalk');
 
 module.exports = function (db) {
-    new CronJob('00 55 23 * * *', function () {
-        const nextExecution = this.nextDates().toString();
-        console.log('TWITTER CRON::STARTED Will fire again at: ' + nextExecution);
+    let job = new CronJob('00 55 23 * * *', function () {
+        let nextExecution = this.nextDates().toString();
+        console.log('TWITTER CRON::STARTED. Will fire again at: ' + nextExecution);
         fetchTweets(db);
     }, null, true, 'Europe/Zurich');
+
+    let nextExecution = job.nextDates().toString();
+    console.log(chalk.green('TWITTER CRON::REGISTERED. Will fire at: ' + nextExecution));
 };
