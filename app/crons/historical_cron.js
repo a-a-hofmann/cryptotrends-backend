474da4cb7cc54ab02c842fd94ef9dafb14de9ee2
Alexander Hofmann <alexhofmann@gmail.com>
2018-05-26 11:39:50 +0200

    Fix cron expressions

--- app/crons/historical_cron.js
+++ app/crons/historical_cron.js
@@ -4,7 +4,7 @@ const { fetchHistoricalData } = require('../services');
 module.exports = function (db) {
     const historicalCollection = db.collection('historical');
 
-    new CronJob('00 55 23 * *', function () {
+    new CronJob('00 55 23 * * *', function () {
         historicalCollection.drop(function (err, delOK) {
             if (delOK) {
                 fetchHistoricalData(db);
--- app/crons/twitter_cron.js
+++ app/crons/twitter_cron.js
@@ -2,7 +2,7 @@ const CronJob = require('cron').CronJob;
 const { fetchTweets } = require('../services');
 
 module.exports = function (db) {
-    new CronJob('00 55 23 * *', function () {
+    new CronJob('00 55 23 * * *', function () {
         console.log('TWITTER CRON::STARTED');
         fetchTweets(db);
     }, null, true, 'Europe/Zurich');
